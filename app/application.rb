44ceca3f6399d6366ea91605379ed7511c954e80
camryn williams <williams.camryn.e@gmail.com>
2021-06-09 11:26:20 -0400

    commit tree b35e182cca83beb0481c4df48b0df0b812354669

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +14,17 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length > 0
+        @@cart.each do |cart_item|
+          resp.write "#{cart_item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+    elsif req.path.match(/add/)
+      add_req = req.params["item"]
+      resp.write handle_add(add_req)
     else
       resp.write "Path Not Found"
     end
@@ -27,4 +39,28 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def handle_add(item)
+    if @@items.include?(item)
+      @@cart << item
+      return "added #{item}"
+    else
+      return "We don't have that item"
+    end
+  end
+
+  # def show_cart
+  #   # @@cart.each do |cart_item|
+  #   #   resp.write "#{cart_item}\n"
+  #   # end
+  #   if @@cart.length > 0
+  #     @@cart.each do |cart_item|
+  #       resp.write "#{cart_item}\n"
+  #     end
+  #   else
+  #     return "Your cart is empty"
+  #   end
+  # end
+
+
 end
